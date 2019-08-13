57acc09e05ae104e53e15c5cae03252a17817ed1
zhangyilin <1357146670@qq.com>
2019-08-13 08:56:45 +0800

    Initial commit

---
+++ src/main/java/org/lanqiao/JdsystemApplication.java
@@ -0,0 +1,13 @@
+package org.lanqiao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdsystemApplication.class, args);
+	}
+
+}
