3de8f4e2419bd869af186d7e4ecd2064f9b677cd
Yinyunpeng <1418744255@qq.com>
2019-08-13 17:35:33 +0800

    登录功能实现（验证+登录跳转）成功登录后跳转页面为百度，用户名要求为4-12个中英文字符，密码为8-20位非汉字字符

---
+++ src/main/java/org/lanqiao/entity/Users.java
@@ -0,0 +1,115 @@
+package org.lanqiao.entity;
+
+import java.util.Date;
+
+public class Users {
+    private Integer userId;
+
+    private Integer cartId;
+
+    private String userName;
+
+    private String phone;
+
+    private String password;
+
+    private String email;
+
+    private String idcard;
+
+    private String sex;
+
+    private String zipCode;
+
+    private Date birthday;
+
+    private String isVip;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getCartId() {
+        return cartId;
+    }
+
+    public void setCartId(Integer cartId) {
+        this.cartId = cartId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public String getIdcard() {
+        return idcard;
+    }
+
+    public void setIdcard(String idcard) {
+        this.idcard = idcard == null ? null : idcard.trim();
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex == null ? null : sex.trim();
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode == null ? null : zipCode.trim();
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getIsVip() {
+        return isVip;
+    }
+
+    public void setIsVip(String isVip) {
+        this.isVip = isVip == null ? null : isVip.trim();
+    }
+}
